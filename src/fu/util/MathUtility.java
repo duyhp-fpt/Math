14fc17a94958cc3420aef8fc9427411d5d3e1dc4
duyhp-fpt <hpduy00vvk@gmail.com>
2020-11-19 08:35:10 +0700

    use recursion and GREEN again

--- src/fu/util/MathUtility.java
+++ src/fu/util/MathUtility.java
@@ -5,7 +5,6 @@ package fu.util;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Huynh Duy
@@ -18,17 +17,32 @@ public class MathUtility {
 
     //hàm tính n giai thừa, n>=0 && n<=20
     //nếu quá 20 tràn kiểu long
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+//        if(n==0||n==1)
+//            return 1;//0! = 1! = 1
+//        //sống sót đến đây, thì n > 1 và <= 20 rồi
+//       long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) 
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be >= 0 & <= 20");
-        if(n==0||n==1)
-            return 1;//0! = 1! = 1
-        //sống sót đến đây, thì n > 1 và <= 20 rồi
-       long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
         }
-        return result;
+        if (n == 0 || n == 1) {
+            return 1;//0! = 1! = 1
+        }
+        return n * getFactorial(n - 1);// đệ quy đó em
+                                       // gọi lại mình với quy mô nhở hon
+        //5! = 5 * 4!
+        //4! = 4 * 3!
+        // ...
+        //2! = 2 * 1! (dừng)
+        //n! = n * (n - 1)!
     }
-    
+
 }
